6904ed6f626253b201129efd976db978f878245e
Jeffrey Ng <jeffng6688@gmail.com>
2018-03-19 17:06:03 -0700

    Refactor DualImage and TripleImage flex props to give gutter space

--- src/components/DualImage.js
+++ src/components/DualImage.js
@@ -4,11 +4,11 @@ const DualImage = () => (
   <section className="dual-image">
     <div className="dual-image__container dual-image__container-1">
       <h2 className="dual-image-title">Wear to Work</h2>
-      {/* <img className="dual-image__img" src="../img/homepage-1.jpg" alt="Wear to Work" /> */}
+      <img className="dual-image__img" src="../img/homepage-double-1.jpg" alt="Wear to Work" />
     </div>
     <div className="dual-image__container dual-image__container-2">
       <h2 className="dual-image-title">#DamnGoodDenim</h2>
-      {/* <img className="dual-image__img" src="../img/homepage-2.jpg" alt="#DamnGoodDenim" /> */}
+      <img className="dual-image__img" src="../img/homepage-double-2.jpg" alt="#DamnGoodDenim" />
     </div>
   </section>
 );
--- src/components/TripleImage.js
+++ src/components/TripleImage.js
@@ -4,12 +4,15 @@ const TripleImage = () => (
   <section className="triple-image">
     <div className="triple-image__container triple-image__container-1">
       <h3 className="triple-image__title">The Kick Crop</h3>
+      <img className="triple-image__img" src="../img/homepage-triple-1.jpg" alt="The Kick Crop"/>
     </div>
     <div className="triple-image__container triple-image__container-2">
       <h3 className="triple-image__title">New Day Heels</h3>
+      <img className="triple-image__img" src="../img/homepage-triple-2.jpg" alt="New Day Heels"/>
     </div>
     <div className="triple-image__container triple-image__container-3">
       <h3 className="triple-image__title">Back in Stock</h3>
+      <img className="triple-image__img" src="../img/homepage-triple-3.jpg" alt="Back in Stock"/>
     </div>
   </section>
 );
